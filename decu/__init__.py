26b998a95a738c922a3c4dd85e6a456fbc6ec17e
leotrs <leo@leotrs.com>
2017-10-05 13:18:47 -0400

    [feat] add experiment decorator. For now, it adds logging functionality

--- decu/__init__.py
+++ decu/__init__.py
@@ -6,8 +6,14 @@ A library for experimental computation scripts.
 
 """
 
+import os
+import time
 import logging
+import inspect
+import functools
 from datetime import datetime
+
+NOW = datetime.now()
 
 
 class Script():
@@ -28,10 +34,37 @@ class Script():
     DATA_PATH = 'data/'
     RESULTS_PATH = 'results/'
     FIGURES_PATH = 'pics/'
+    LOGS_PATH = 'logs/'
 
 
     def __init__(self, mode='devel'):
         self.mode = mode
 
+        try:
+            filename = inspect.getfile(self.__class__)
+        except TypeError as exc:
+            # When defining a subclass on the interpretr, the subclass will
+            # have no file attached.
+            filename = '__main__'
+        _, script_name = os.path.split(filename)
+        path = os.getcwd()
+        self.logfile = '{}_{}.txt'.format(NOW, script_name)
+        self.logfile = os.path.join(path, self.LOGS_PATH, self.logfile)
+        logging.basicConfig(level=logging.INFO, filename=self.logfile)
+
     def main(self, *args, **kwargs):
         """Override this method with the main contents of the script."""
+
+
+def experiment(method):
+    """Decorator that adds logging functionality to experiment methods."""
+    @functools.wraps(method)
+    def decorated(*args, **kwargs):
+        logging.info('Starting experiment..')
+        start = time.time()
+        result = method(*args, **kwargs)
+        end = time.time()
+        logging.info('Finished experiment. Took {}'.format(end - start))
+        return result
+
+    return decorated
