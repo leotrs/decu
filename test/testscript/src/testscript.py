7d1ff5e46ee31ce48b2748e87240f96c10bd963a
leotrs <leo@leotrs.com>
2017-10-19 15:39:30 -0400

    [refactor] the new testscript.py version actually respects the contract that the @figure-decorated method should accept as parameter the exact same object output by a @experiment-decorated method.

--- test/testscript/src/testscript.py
+++ test/testscript/src/testscript.py
@@ -18,22 +18,26 @@ class TestScript(Script):
     def exp(self, data, param):
         """Compute x**param for each data point."""
         logging.info('Working hard for {}..'.format(param))
-        return np.array([np.power(x, param) for x in data])
+        return np.power(data, param)
 
     @figure()
-    def plot_results(self, data, result):
+    def plot_result(self, data, result):
+        """Plot results of experiment."""
+        plt.plot(data, result)
+
+    @figure()
+    def plot_many_results(self, data, results):
         """Plot results of experiment."""
         plt.figure()
-        for param in result.keys():
-            plt.plot(data, result[param])
+        for param in results.keys():
+            plt.plot(data, results[param])
 
     def main(self):
         """Run some experiments and make some figures."""
         data = np.arange(5)
-        param_list1 = np.arange(5)
-        result1 = {p: self.exp(data, param=p) for p in param_list1}
-        self.plot_results(data, result1)
+        result1 = self.exp(data, param=4)
+        self.plot_result(data, result1)
 
-        param_list2 = np.arange(5, 10)
-        result2 = run_parallel(self.exp, data, param_list2)
-        self.plot_results(data, result2, suffix='parallel')
+        param_list = np.arange(5, 10)
+        result2 = run_parallel(self.exp, data, param_list)
+        self.plot_many_results(data, result2, suffix='parallel')
