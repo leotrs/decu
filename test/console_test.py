ce5a8b3c7e780200fcf2b4e95357e5aa45a307bb
leotrs <leo@leotrs.com>
2017-10-19 11:32:33 -0400

    [test] some console commands should give an error when called without arguments. Fixes #40.

--- test/console_test.py
+++ test/console_test.py
@@ -6,8 +6,10 @@ Test the console scripts.
 
 """
 
+import pytest
 import decu
 import decu.__main__ as main
+from subprocess import check_call, CalledProcessError
 
 
 def test_init(tmpdir):
@@ -19,3 +21,19 @@ def test_init(tmpdir):
     main.init(str(tmpdir))
     for name in dir_names:
         assert name in tmpdir.listdir()
+
+
+def test_exec_no_arg():
+    """Test that `decu exec` without an argument gives an error."""
+    # Error code 2 means 'misuse of shell built-ins' and is the code that
+    # argparse gives in case there's an parsing error.
+    with pytest.raises(CalledProcessError):
+        check_call(['decu', 'exec'])
+
+
+def test_inspect_no_arg():
+    """Test that `decu inspect` without an argument gives an error."""
+    # Error code 2 means 'misuse of shell built-ins' and is the code that
+    # argparse gives in case there's an parsing error.
+    with pytest.raises(CalledProcessError):
+        check_call(['decu', 'inspect'])
