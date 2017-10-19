939a8d9f372162d4e94da1727bccf01fb18625c1
leotrs <leo@leotrs.com>
2017-10-19 09:18:08 -0400

    [feat] add `decu inspect` functionality WIP #30. [refactor] moved some imports to inside functions in __main__

--- decu/__main__.py
+++ decu/__main__.py
@@ -7,8 +7,6 @@ Main decu executable.
 """
 
 import os
-import sys
-import importlib
 import decu
 from datetime import datetime
 NOW = datetime.now()
@@ -30,10 +28,12 @@ def extract_script_class(module):
 
 def exec_script(path):
     """Execute the main function inside a file."""
+    import sys
+    from importlib import import_module
     module_path, module_file = os.path.split(path)
     sys.path.append(make_absolute_path(module_path))
     module_name, _ = os.path.splitext(module_file)
-    module = importlib.import_module(module_name)
+    module = import_module(module_name)
     script = extract_script_class(module)(NOW, os.getcwd(), module_file)
     script.main()
 
@@ -49,11 +49,20 @@ def init(path):
     print('Initialized empty decu project directory in {}'.format(path))
 
 
+def inspect(path):
+    """Load a result file and go into ipython."""
+    from subprocess import call
+    py_cmd = 'import decu;import numpy as np;results = np.loadtxt("{}")'.format(path)
+    cli_cmd = ['ipython', '-i', '--no-banner', '-c', '\'{}\''.format(py_cmd)]
+    print(py_cmd.replace(';', '\n'))
+    call(cli_cmd)
+
+
 def main():
     """Execute the script passed as command line argument."""
     from argparse import ArgumentParser, ArgumentError
     parser = ArgumentParser(description='Experimental computation utilities.')
-    parser.add_argument('mode', choices=['init', 'exec'])
+    parser.add_argument('mode', choices=['init', 'exec', 'inspect'])
     parser.add_argument('path', nargs='?', help='the script to be run')
     args = parser.parse_args()
 
@@ -64,7 +73,11 @@ def main():
             exec_script(args.path)
     elif args.mode == 'init':
         init(os.getcwd())
-
+    elif args.mode == 'inspect':
+        if args.path is None:
+            parser.error('path must be specified when using inspect')
+        else:
+            inspect(args.path)
 
 
 if __name__ == "__main__":
