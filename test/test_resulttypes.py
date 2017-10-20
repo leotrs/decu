7f7d0d44bf31f9a3ec2afc1f64b73e08356b7058
leotrs <leo@leotrs.com>
2017-10-20 16:03:42 -0400

    [fix] add a dummy data_param, for now

--- test/test_resulttypes.py
+++ test/test_resulttypes.py
@@ -16,7 +16,7 @@ from decu import experiment, read_result
 def test_array(tmpdir):
     """Test reading and writing and array."""
     class TestArray(util.TestScript):
-        @experiment(data_param=None)
+        @experiment(data_param='size')
         def exp(self, size):
             return np.arange(size)
 
