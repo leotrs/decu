9f5e80d61eecc094da75cb10063f13a551f4fce9
leotrs <leo@leotrs.com>
2017-10-21 21:04:09 -0400

    [test] add console tests and provide better docstrings. Fixes #63.

--- test/console_test.py
+++ test/console_test.py
@@ -13,7 +13,7 @@ from subprocess import check_call, CalledProcessError
 
 
 def test_init(tmpdir):
-    """Test the `decu init` command."""
+    """`decu init` should create the appropriate directories."""
     dir_names = [tmpdir.join(v) for k, v in config['Script'].items()
                  if k.endswith('_dir')]
     for name in dir_names:
@@ -23,15 +23,36 @@ def test_init(tmpdir):
         assert name in tmpdir.listdir()
 
 
+def test_already_initialized_init(tmpdir):
+    """`decu init` on an already initialized project shouldn't harm it."""
+    dir_names = [tmpdir.join(v) for k, v in config['Script'].items()
+                 if k.endswith('_dir')]
+    main.init(str(tmpdir))
+
+    test_dir = tmpdir.join(config['Script']['scripts_dir'])
+    test_filename = test_dir.join('foo.txt')
+    with test_filename.open('w+') as file:
+        file.write('this is a test')
+
+    main.init(str(tmpdir))
+    for name in dir_names:
+        assert name in tmpdir.listdir()
+    assert test_filename in test_dir.listdir()
+
+
 def test_exec_no_arg():
-    """Test that `decu exec` without an argument gives an error."""
+    """`decu exec` without an argument should give an error."""
     with pytest.raises(CalledProcessError):
         check_call(['decu', 'exec'])
 
 
-def test_inspect_no_arg():
-    """Test that `decu inspect` with wrong arguments gives an error."""
+def test_inspect_wrong_args():
+    """`decu inspect` with wrong arguments should give an error."""
     with pytest.raises(CalledProcessError):
         check_call(['decu', 'inspect'])
     with pytest.raises(CalledProcessError):
         check_call(['decu', 'inspect', 'testscript/src/testscript.py', '--plot'])
+    with pytest.raises(CalledProcessError):
+        check_call(['decu', 'inspect', 'testscript/src/testscript.py', '--data'])
+    with pytest.raises(CalledProcessError):
+        check_call(['decu', 'inspect', 'testscript/src/testscript.py', '--data', 'foo', '--bar'])
