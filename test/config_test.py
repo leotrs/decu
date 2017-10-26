bd7bd6c5ba907252c0da8c172ca633454e0cfc6b
leotrs <leo@leotrs.com>
2017-10-26 15:11:51 -0400

    [refactor] rewrite the test to check local config file takes precedence over global one - also makes config_test.py play well in a pytest session, so that we no longer need test/all_tests.py, test/Makefile, and simplifies .travis.yml

--- test/config_test.py
+++ test/config_test.py
@@ -7,22 +7,31 @@ Test local config files.
 """
 
 import os
-import pytest
+import decu
+from decu import __main__ as main
 
 
-@pytest.mark.isolated
-def test_local_override():
+# @pytest.mark.isolated
+def test_local_override(tmpdir):
     """Test whether a local file overrides default options."""
     config_filename = 'decu.cfg'
-    assert config_filename not in os.listdir()
-
     new_opt = '${time}.log'
-    config = '[Script]\nlog_file = {}'.format(new_opt)
-    with open(config_filename, 'w+') as cfg_file:
-        cfg_file.write(config)
+    config = '[logging]\nlog_file = {}'.format(new_opt)
 
-    import decu
-    try:
-        assert decu.config['Script']['log_file'] == new_opt
-    finally:
-        os.remove(config_filename)
+    main.init(str(tmpdir))
+
+    with tmpdir.join(config_filename).open('w+') as file:
+        file.write(config)
+
+    script = 'import decu\nprint(decu.config["logging"]["log_file"])'
+    script_file = tmpdir.join(decu.config['Script']['scripts_dir']).join(
+        'script.py')
+    with script_file.open('w+') as file:
+        file.write(script)
+
+    from subprocess import check_output
+    curdir = os.getcwd()
+    os.chdir(str(tmpdir))
+    output = check_output(['python', str(script_file)])
+    assert output.decode().strip() == new_opt
+    os.chdir(curdir)
