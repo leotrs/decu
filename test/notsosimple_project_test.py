552ff7375442e71f058a5846e757a4386baa2caa
leotrs <leo@leotrs.com>
2017-10-27 09:40:03 -0400

    debugging travis build - will revert later

--- test/notsosimple_project_test.py
+++ test/notsosimple_project_test.py
@@ -20,6 +20,10 @@ teardown = pytest.fixture(scope='module', autouse=True)(
 def test_exec():
     """Test that `decu exec src/script.py` generates the appropriate files."""
     cfg = decu.config['Script']
+
+    print(decu.config['logging']['logs_dir'])
+    print(cfg['figures_dir'])
+
     call(['decu', 'exec', '{}/script.py'.format(cfg['scripts_dir'])])
     assert os.listdir(decu.config['logging']['logs_dir'])
     assert os.listdir(cfg['figures_dir'])
