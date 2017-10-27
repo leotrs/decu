3425d671482032b8507af67c6102b596b6170b2f
leotrs <leo@leotrs.com>
2017-10-27 09:51:33 -0400

    [fix] make sure that logfile exists - should fix travis builds

--- decu/logging.py
+++ decu/logging.py
@@ -35,6 +35,7 @@ class DecuLogger():
                                          module_name=module)
 
         logfile = os.path.join(project_dir, self.logs_dir, logfile)
+        os.makedirs(logfile, exist_ok=True)
         logger = logging.getLogger(logfile)
         logger.setLevel(logging.INFO)
         handler = logging.FileHandler(logfile)
